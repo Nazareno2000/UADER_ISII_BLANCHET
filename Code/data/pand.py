5dbd56621f4169cbb15740e4477e44882a4f2d3e
Nazareno <nachi10@hotmail.com>
2022-04-13 19:22:27 -0300

    trabajo jupyter

--- Code/data/pand.py
+++ Code/data/pand.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv("Desktop\Code\data\dataset.csv",index_col="id")
+df = pd.read_csv("dataset.csv",index_col="id")
 print(df)
 print(df.describe())
 print((df.head))
